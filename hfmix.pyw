46f2a43ffcfa1b0c60204c373741b70f484ca047
danipj <daniela.palumbo@outlook.com>
2016-05-12 01:06:50 -0300

    Added comments

--- hfmix.pyw
+++ hfmix.pyw
@@ -2,16 +2,31 @@ from Tkinter import *
 from sound_panel import *
 import pygame.mixer
 import os
+
+#inicia a aplicação gráfica
 app = Tk()
-app.title('Head First Mix')
+app.title('DrumPad PyLadies')
+
+#http://www.pygame.org/docs/ref/mixer.html
+#inicia a parte de sons do pygame
 mixer = pygame.mixer
 mixer.init()
-dirList = os.listdir(".")
-for fname in dirList:
+
+#pega o diretório atual (.)
+files = os.listdir(".")
+
+#inicia um SoundPanel para todos os arquivos terminados em .aif
+for fname in files:
     if fname.endswith('.aif'):
         SoundPanel(app, mixer, fname).pack()
+
+#função que encerra a aplicação
 def termina():
     mixer.stop()
     app.destroy()
+
+#define que quando clicado no X, chama a função acima
 app.protocol('WM_DELETE_WINDOW', termina)
+
+#inicia execução
 app.mainloop()
--- sound_panel.py
+++ sound_panel.py
@@ -1,26 +1,45 @@
 from Tkinter import *
 import pygame.mixer
+
 class SoundPanel(Frame):
-    def muda(self):
+
+    def on_off(self):
         if self.tocando.get():
+            #loops -1 = tocar pra sempre
             self.track.play(loops = -1)
         else:
             self.track.stop()
+
     def muda_volume(self, v):
         self.track.set_volume(self.volume.get())
+
     def __init__(self, app, mixer, sound_file):
         Frame.__init__(self, app)
+
+        #http://www.pygame.org/docs/ref/mixer.html#pygame.mixer.Sound
+        #cria um objeto de som, preferencialmente OGG ou WAV. suporte MP3 limitado
         self.track = mixer.Sound(sound_file)
+
+        #cria uma variavel numerica pra ser setada no botao
         self.tocando = IntVar()
-        tocar = Checkbutton(self, variable = self.tocando,
-                            command = self.muda,
+
+        #cria um botao de check pra definir on_off
+        botaoTocar = Checkbutton(self, variable = self.tocando,
+                            command = self.on_off,
                             text = sound_file)
-        tocar.pack(side = LEFT)
+        #posiciona o botao na tela
+        botaoTocar.pack(side = RIGHT)
+
+        #cria uma variavel numerica pra armazenar volume
         self.volume = DoubleVar()
+        #define ela como o volume da faixa
         self.volume.set(self.track.get_volume())
+
+        #cria um botao de escala pra definir volume
         escala = Scale(self, variable = self.volume,
             from_ = 0.0, to = 1.0, resolution = 0.1,
             command = self.muda_volume,
             label = 'Volume', orient = HORIZONTAL)
+        #posiciona o botao na tela
         escala.pack(side = RIGHT)
 
